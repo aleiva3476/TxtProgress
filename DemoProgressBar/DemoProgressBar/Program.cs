aaaee43d83de1ff5ad6f17df39b0b5a1986bbbea
Antonio Leiva <antonio.leiva@gmail.com>
2017-06-08 13:37:49 +0200

    Initial version. TxtProgress and demo.

---
+++ DemoProgressBar/DemoProgressBar/Program.cs
@@ -0,0 +1,39 @@
+﻿using Alp3476;
+using System;
+using System.Diagnostics;
+
+namespace DemoProgressBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            Console.WindowWidth = 100;
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 1; i <= 5; i++)
+            {
+                int numPasos = r.Next(20, 50);
+                string titulo = $"Proceso nº {i}";
+
+                TxtProgress.Start(titulo, numPasos);
+                for (int j = 0; j < numPasos; j++)
+                {
+                    TxtProgress.Step();
+                    System.Threading.Thread.Sleep(50);
+                }
+                TxtProgress.End();
+            }
+
+            sw.Stop();
+
+            Console.WriteLine($"\r\nTiempo total: {sw.Elapsed}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoProgressBar/DemoProgressBar/TxtProgress.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Alp3476
+{
+    class TxtProgress
+    {
+        const int LEN_TITLE = 20;
+        const int TOTAL_BLOCKS = 30;
+
+        private static string title;
+        private static int numSteps = 0;
+        private static int current = 0;
+        private static Stopwatch sw = new Stopwatch();
+
+        public static void Start(string title, int numSteps)
+        {
+            TxtProgress.title = title;
+            TxtProgress.numSteps = numSteps;
+            if (TxtProgress.title.Length > LEN_TITLE)
+            {
+                TxtProgress.title = TxtProgress.title.Substring(0, LEN_TITLE);
+            }
+            current = 0;
+
+            sw.Reset();
+            sw.Start();
+
+            Console.CursorVisible = false;
+        }
+
+        public static void Step()
+        {
+            ++current;
+            decimal percent = Math.Ceiling(numSteps == 0 ? 100 : current * 100M / (decimal)numSteps);
+            int numBlocks = (int)(TOTAL_BLOCKS * percent / 100M);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"\r{title,-LEN_TITLE} ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write($"{percent,6:N0}%  ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(new string('\x2588', numBlocks));
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(new string('\x2591', TOTAL_BLOCKS - numBlocks));
+        }
+
+        public static void End()
+        {
+            sw.Stop();
+
+            /* Force repainting with 100 % */
+            current = numSteps - 1;
+            Step();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" OK ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("(");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(sw.Elapsed);
+
+            Console.ResetColor();
+            Console.WriteLine($") {numSteps,6:N0} reg.");
+
+            Console.CursorVisible = true;
+        }
+    }
+}
